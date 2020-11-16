03fc136ce425b96dfadd41505b322b0835c59812
Christopher Todd Forsyth <christforsyth@ln003.brc>
2020-11-15 17:25:52 -0800

    Added benchmark input files and bash scripts for U233-MET-FAST-001, U233-MET-FAST-002, and PU-MET-FAST-005

---
+++ PU-MET-FAST-005/PU-MET-FAST-005.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=PU-MET-FAST-005
+#
+#SBATCH --output=outputPU-MET-FAST-005
+#
+#SBATCH --error=ERRORS
+#
+# Account:
+#
+#SBATCH --qos=nuclear_savio_normal
+#
+#SBATCH --account=co_nuclear
+#
+# Partition:
+#SBATCH --partition=savio
+#
+# Request one node:
+#SBATCH --nodes=1
+#
+# Request cores (20, for example)
+#SBATCH --ntasks-per-node=1
+#
+# Processors per task:
+#SBATCH --cpus-per-node=20
+#
+# Wall clock limit:
+#SBATCH --time=00:10:00
+#
+#this will send me an email
+#SBATCH --mail-user=chris.t.forsyth@berkeley.edu
+#SBATCH --mail-type=all
+#
+#
+## Command(s) to run (example):
+cd /global/home/users/christforsyth/ML_neutronics/PU-MET-FAST-005
+sss2 PU-MET-FAST-005 -omp 20
